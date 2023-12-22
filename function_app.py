571ec91adaa66905146f944928025a4600212cf8
Juha Tervo <jutervo@outlook.com>
2023-12-22 15:14:01 +0200

    Changes to be committed: 	modified:   function_app.py

--- function_app.py
+++ function_app.py
@@ -1,10 +1,23 @@
 import azure.functions as func
 import logging
 
+# specify connection string to your event hubs namespace and
+# the event hub name
+producer = EventHubProducerClient.from_connection_string(
+    conn_str="EVENT HUBS NAMESPACE - CONNECTION STRING",
+    eventhub_name="EVENT HUB NAME"
+)
+
 app = func.FunctionApp()
 
-@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="deviotopstervo01",
+
+
+@app.event_hub_message_trigger(arg_name="customer_eventhub", event_hub_name="deviotopstervo01",
                                connection="CONNECTIONSTRING") 
 def eventhub_trigger(azeventhub: func.EventHubEvent):
     logging.info('Python EventHub trigger processed an event: %s',
-                azeventhub.get_body().decode('utf-8'))
+                customer_eventhub.get_body().decode('utf-8'))
+    # process the event
+
+    # forward the event to internal eventHub
+    azeventhub.
